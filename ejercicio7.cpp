92c9fd93547d7135717949fd65a2c46643d7cf74
Federico González <03.federico.gonzalez@gmail.com>
2024-05-25 20:55:08 -0300

    ej7

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -7,6 +7,33 @@ using namespace std;
 
 int main()
 {
-    // TODO
-    return 0;
+
+    int potencia = 0;
+    cin >> potencia;
+    cin.ignore();
+
+    int potenciaActual = potencia;
+
+    int n = 0;
+    cin >> n;
+    cin.ignore();
+
+    for (int i = 0; i < n; i++)
+    {
+        int virus;
+        cin >> virus;
+        cin.ignore();
+        if (virus <= potenciaActual || virus <= potencia)
+        {
+            potencia += virus;
+            potenciaActual += virus;
+        }
+        else
+        {
+            potenciaActual -= virus;
+        }
+    }
+
+    bool puedeDefenderse = potenciaActual >= 0;
+    cout << (puedeDefenderse ? "true" : "false") << endl;
 }
